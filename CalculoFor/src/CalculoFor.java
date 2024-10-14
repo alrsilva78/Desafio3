264d9dde49ba1ed06b55b41e2a4b63b74237e79a
Oraclle78 <informaticaalrsilva@hotmail.com>
2024-10-14 15:49:33 -0300

    Desafio 3 V1

---
+++ CalculoFor/src/CalculoFor.java
@@ -0,0 +1,17 @@
+public class CalculoFor {
+
+    public static int calculo () {
+
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k < indice) {
+            k = k + 1;
+            soma = soma + k;
+            
+        }
+        return soma;
+    }
+
+}
---
+++ CalculoFor/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("O resultado é igual a " + CalculoFor.calculo());;
+    }
+}
